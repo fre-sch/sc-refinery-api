dbeefeb38f3060c27e7e59f5a78e2bd2b27b2718
Frederik Schumacher <fre-sch@users.noreply.github.com>
2022-04-07 09:00:05 +0200

    remove old files

--- screfinery/storage/__init__.py
+++
@@ -1,7 +0,0 @@
-from screfinery.storage.method import MethodStore, method_validate
-from screfinery.storage.mining_session import MiningSessionStore, mining_session_validate
-from screfinery.storage.ore import OreStore, ore_validate
-from screfinery.storage.station import StationStore, station_validate
-from screfinery.storage.user import UserStore, user_validator, login_validator
-from screfinery.storage.user_perm import UserPermStore
-from screfinery.storage.user_session import UserSessionStore
--- screfinery/storage/user_session.py
+++
@@ -1,7 +0,0 @@
-from screfinery.basestore import BaseStore
-from screfinery.storage.table import UserSession
-
-
-class UserSessionStore(BaseStore):
-    def __init__(self):
-        super().__init__(UserSession)
